07dcbbe4f1c2b5257efca3cc21054ea799e4caf9
SaskyaPanchaud <saskya.panchaud@heig-vd.ch>
2023-11-25 15:36:13 +0100

    wip

--- src/main/java/dai/smtp/client/EmailGroup.java
+++ src/main/java/dai/smtp/client/EmailGroup.java
@@ -3,21 +3,37 @@ package dai.smtp.client;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class EmailGroup {
-    String[] emails;
+
+    static int MAX_NB_RECEIVERS = 5;
+    int nbAdr;
     String sender;
     String[] receivers;
 
     public EmailGroup(File emailsList) {
-        // TODO : verifier que minimum 2 adresses dans fichier des victimes
-        // TODO : selectionner entre 2 - 5 adresses, assigner premiere a expediteur et reste a destinataire(s)
-        this.emails = toStringEmails(emailsList);
-        this.sender = chooseSender();
+        // TODO : controler le contenu du fichier -> si @ dans chaque ligne + verifier que minimum 2 adresses dans fichier des victimes
+        this.nbAdr = nbAdresses(emailsList);
+        this.sender = chooseSender(emailsList);
         this.receivers = chooseReceivers();
     }
 
-    private String[] toStringEmails(File emailsList) {
+    private int nbAdresses(File emailsList) {
+        int count = 0;
+        try {
+            LineNumberReader lnr = new LineNumberReader(new FileReader(emailsList));
+            while (lnr.readLine() != null) {
+                ++count;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Problem while counting the number of adresses...");
+        }
+        return count;
+    }
+
+    // TODO : supprimer si plus utile
+    /*private String[] toStringEmails(File emailsList) {
         ArrayList<String> emails = new ArrayList<>();
         try (var in = new BufferedReader(new InputStreamReader(new FileInputStream(emailsList), StandardCharsets.UTF_8))) {
             String line;
@@ -28,15 +44,37 @@ public class EmailGroup {
             throw new RuntimeException("Bad adresses file...");
         }
         return emails.toArray();
-    }
+    }*/
 
-    private String chooseSender() {
-        return emails[RandomInt()];
+    private String chooseSender(File emailsList) {
+        try (var in = new BufferedReader(new InputStreamReader(new FileInputStream(emailsList), StandardCharsets.UTF_8))) {
+            String line;
+            StringBuilder sender = new StringBuilder();
+            Random rdm = new Random();
+            int indexRandom = rdm.nextInt(nbAdr + 1);
+            int i = 0;
+            while ((line = in.readLine()) != null) {
+                if (i == indexRandom) {
+                    sender.append(line);
+                    break;
+                }
+                ++i;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Problem while choosing the sender...");
+        }
+        return sender.toString();
     }
 
     private String[] chooseReceivers() {
         ArrayList<String> receivers = new ArrayList<>();
-        // TODO
-        return receivers.toArray();
+        Random rdm = new Random();
+        int nbReceivers = rdm.nextInt(MAX_NB_RECEIVERS) + 1;
+        for (int i = 0; i < nbReceivers; ++i) {
+            // FIXME : ajouter un controle pour ne pas choisir plusieurs fois la meme adresse ?
+            // TODO
+            receivers.add(line);
+        }
+        return receivers.toArray(new String[receivers.size()]);
     }
 }

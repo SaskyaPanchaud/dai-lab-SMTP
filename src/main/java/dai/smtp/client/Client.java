d1bb7bbb3a4bf7f2ea0a1951cbf434042d654826
SaskyaPanchaud <saskya.panchaud@heig-vd.ch>
2023-11-25 14:56:54 +0100

    clean Client

--- src/main/java/dai/smtp/client/Client.java
+++ src/main/java/dai/smtp/client/Client.java
@@ -4,34 +4,33 @@ import java.io.*;
 
 public class Client {
     public static void main(String[] args) {
-
-        // controles des args
         if (args.length != 3) {
             throw new RuntimeException("Required args: <victims_list_path> <messages_list_path> <nGroups>.");
         }
-        // TODO : verfier si chemin valide
-        if (args[0] && args[1]) {
-            throw new RuntimeException("Filepath not valid...");
-        }
-        // TODO : verfier si type = int
-        if (args[2]) {
-            throw new RuntimeException("Arg for number of groups doesn't match.");
+
+        int numberGroups;
+        File emailsList;
+        File messagesList;
+
+        try {
+            numberGroups = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Give a number for the third arg (number of groups).");
         }
 
-        // stockage des args
-        File emailsList = new File(args[0]);
-        File messagesList = new File(args[1]);
-        int numberGroups = Integer.parseInt(args[2]);
+        try {
+            emailsList = new File(args[0]);
+            messagesList = new File(args[1]);
+        } catch (Exception e) {
+            throw new RuntimeException("Give filepaths valid.");
+        }
 
-        // traitement du nombre de groupe souhaite
         for (int n = 0; n < numberGroups; ++n) {
-            // TODO : controle du contenu des args dans chaque classe
             // formation du groupe
             EmailGroup group = new EmailGroup(emailsList);
             // attribution du message
             EmailContent content = new EmailContent(messagesList);
             // envoi du message
-            // TODO : s'occuper de l'encodage du header
             new EmailSend(group, content);
         }
     }

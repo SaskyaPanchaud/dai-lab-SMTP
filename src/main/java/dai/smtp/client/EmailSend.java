35fcd87caf1e609d9758a09686bbf735a42e78a9
SaskyaPanchaud <saskya.panchaud@heig-vd.ch>
2023-11-26 12:23:12 +0100

    EmailSend almost ok

--- src/main/java/dai/smtp/client/EmailSend.java
+++ src/main/java/dai/smtp/client/EmailSend.java
@@ -11,89 +11,83 @@ public class EmailSend {
     static final int SERVER_PORT = 1025;
 
     public boolean send(ArrayList<String> adresses, HashMap<String, String> message) {
+        // definition du sender et des receivers
         String sender = adresses.get(0);
         ArrayList<String> receivers = new ArrayList<>();
         for (int i = 1; i < adresses.size(); ++i) {
             receivers.add(adresses.get(i));
         }
 
-
-
-
-        // TODO : s'occuper de l'encodage du header
-
-        // TODO : implementer protocole SMTP
+        // debut conversation avec serveur
         try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT); var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8)); var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8))) {
+            // lecture premier message
             String firstResponse = in.readLine();
             if (firstResponse.startsWith("2")) {
                 System.out.println("Successfully connected to SMTP server");
             } else {
                 throw new RuntimeException("Could't connect to SMTP server. Response: " + firstResponse);
             }
-
-            // TODO : finir envois et reponses avec serveur
-
-            // contacter serveur
-            out.write("ehlo");
+            // envoi premier message
+            out.write("EHLO" + SERVER_ADDRESS + "\n");
             out.flush();
-
-            String s;
-            // lire et afficher lignes des extensions
-            while((s = in.readLine()) != null) {
-                if (s.substring(0,4) != "250 ") {
-                    System.out.println(s);
+            // lecture extensions
+            String extension;
+            while((extension = in.readLine()) != null) {
+                if (extension.substring(0,4) != "250 ") {
+                    System.out.println(extension);
                 } else {
                     break;
                 }
             }
-
-            // envoyer sender
-            out.write(sendToServer("mail from: ", sender));
+            // envoi "mail from" + lecture reponse
+            out.write("mail from: <" + sender + ">\n");
             out.flush();
-            // lire reponse
-            String resultSender;
-            while((resultSender = in.readLine()) != null) {
-                System.out.println(resultSender);
+            String resultSender = in.readLine();
+            if (resultSender.startsWith("2")) {
+                System.out.println("Sender OK");
+            } else {
+                throw new RuntimeException("Sender KO : " + resultSender);
             }
 
-            // envoyer receivers
-            StringBuilder receiversList = new StringBuilder();
-            for (var r : receivers) {
-                receiversList.append(r);
-                receiversList.append("; ");
-            }
-            out.write(sendToServer("rcpt to: ", receivers));
-            // lire reponse
-            String resultReceivers;
-            while((resultReceivers = in.readLine()) != null) {
-                System.out.println(resultReceivers);
-            }
-
-            // envoyer data
-            out.write("data");
-
-            // From
-            out.write("From " + sender + "\n");
+            // envoi "rcpt to" + lecture reponse
+            // FIXME : comment mettre plusieurs receivers ?, tester avec ncat
+            out.write("rcpt to: <" + receivers + ">\n");
             out.flush();
-            // To
+            String resultReceivers = in.readLine();
+            if (resultReceivers.startsWith("2")) {
+                System.out.println("Receivers OK");
+            } else {
+                throw new RuntimeException("Receivers KO : " + resultReceivers);
+            }
 
-            // Date
+            // envoi "data" + lecture reponse
+            out.write("data\n");
+            System.out.println(in.readLine());
 
-            // Subject
-               out.write(message.get)
-            // texte et terminer avec logne.ligne
-            out.write(message.entrySet().)
+            // envoi contenu email
+            out.write("From: <" + sender + ">\n");
+            // FIXME : comment envoyer la liste ?, a tester
+            out.write("To: <" + receivers + ">\n");
+            out.write("Date: " + java.time.LocalDate.now() + "\n");
+            // TODO : s'occuper de l'encodage du header
+            out.write("Subject: " + message.keySet() + "\n");
+            out.write("\n");
+            out.write(message.values() + "\n");
+            out.write("\n.\n");
+            out.flush();
+            // TODO des que tests ok
+            // lire reponse
 
-            // envoyer fin
+
+            // envoi fin
             out.write("quit" + "\n");
             out.flush();
-
             // lire reponse
-
 
         } catch (IOException e) {
             System.out.println("EmailSend: exception while using socket: " + e);
         }
+        return true;
     }
 
     private String sendToServer(String smtpKeyword, String content) {

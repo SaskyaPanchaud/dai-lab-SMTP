27d12238ff4b4ba1c778d93433c8b4a7443f85fb
Alessio Giuliano <giuliano.alessio36@gmail.com>
2023-12-02 11:24:46 +0100

    Added SMTP class for sending messages following the SMTP protocol

--- src/main/java/dai/smtp/client/Client.java
+++ src/main/java/dai/smtp/client/Client.java
@@ -35,7 +35,10 @@ public class Client {
             var in = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
             var out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
         ) {
-
+            SMTP smtp = new SMTP(in, out);
+            smtp.waitAccept();
+            smtp.sendHello(SERVER_ADDRESS);
+            smtp.sendQuit();
         }
         catch (IOException e) {
                 System.out.println("Client IOException: %s".formatted(e.getMessage()));
